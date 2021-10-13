404c30cb1f192bb72195e35d088b8dfb3e5d3716
Michell Rezende de Paula e Silva <michell.rezende.p@gmail.com>
2021-10-13 14:28:13 -0300

    Atualização - função gettimeofday
    Inclui a função gettimeofday do PHP

--- php_fundamental/fundamental/unidade12/ex01-getdate.php
+++ php_fundamental/fundamental/unidade12/ex01-getdate.php
@@ -22,5 +22,15 @@
                 print_r ($_time);
             ?>
         </pre>
+
+        <pre>
+            <?php
+                // carimbo digital para ações, como acesso a uma página ou realização de uma compra, com precisão numérica de milisegundos para maior confiabilidade
+                print_r(gettimeofday());
+                // o dado referente aos milisegundos ocupa a posição [usec]
+                // os valores podem aparecer ligeiramente diferentes, pois o computador também leva um certo tempo para executar as instruções
+                echo gettimeofday()["usec"];
+            ?>
+        </pre>
     </body>
 </html>
